cffb97c5dd6ab845b11053f08ef287ff34640952
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-08-18 18:26:16 -0400

    Made bubble_sort method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,18 +1,15 @@
 arr = [10, 30, 55, 98, 44, 31]
 
-
-
 def bubble_sort(array) 
-    for i in 0..(array.length-1)
-    array[i] <=> array[i+1]
-    if array[i] > array[i+1]
-        deleted = array.delete_at(i)
-        array.insert(i+1, deleted)
-        i += 1
-    else 
-        i += 1
+    (array.length).times do
+     for i in 0...(array.length - 1)
+        if array[i] > array[i + 1] 
+          deleted = array.delete_at(i)
+          array.insert(i+1, deleted)
+        end 
     end
-end
+    end
+    puts array
 end
 
-bubble_sort(arr)
+bubble_sort(a)
