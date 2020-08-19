809c419105c573c980562bfef70a749b1d272828
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-08-19 13:02:56 -0400

    Start bubble_sort_by

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -12,4 +12,23 @@ def bubble_sort(array)
     puts array
 end
 
-bubble_sort(a)
+bubble_sort(arr)
+
+def bubble_sort_by(array) 
+
+  (array.length).times do
+    for i in 0...(array.length - 1)
+      left = array[i]
+      right = array[i + 1]
+       if yield(left,right) > 0
+         deleted = array.delete(left)
+         array.insert(i + 1, deleted)
+       end 
+    end
+  end
+  puts array
+end
+
+  bubble_sort_by(["hi", "hello", "hey"]) do |left,right|
+    left.length - right.length
+  end
