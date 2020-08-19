d4ba79c48d488594bdae1e1a5e4b4fec31566cf4
SunnySparks <josediazt6@outlook.com>
2020-08-19 12:57:51 -0500

    Fix Rubocop issues and delete offenses

---
+++ bubble_sort.rb
@@ -0,0 +1,31 @@
+def bubble_sort(array)
+  array.length.times do
+    (0...(array.length - 1)).each do |i|
+      if array[i] > array[i + 1]
+        deleted = array.delete_at(i)
+        array.insert(i + 1, deleted)
+      end
+    end
+  end
+  puts array
+end
+
+bubble_sort([10, 30, 55, 98, 44, 31])
+
+def bubble_sort_by(array)
+  array.length.times do
+    (0...(array.length - 1)).each do |i|
+      left = array[i]
+      right = array[i + 1]
+      if yield(left, right).positive
+        deleted = array.delete(left)
+        array.insert(i + 1, deleted)
+      end
+    end
+  end
+  puts array
+end
+
+bubble_sort_by(%w[what is up yo]) do |left, right|
+  left.length - right.length
+end
